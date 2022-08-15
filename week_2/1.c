466427e5ea13c8c4926d8d6803b51458fe8b02b3
shoguncoffee <63220735+shoguncoffee@users.noreply.github.com>
2022-08-15 22:58:35 +0700

    Update 1.c

--- week_2/1.c
+++ week_2/1.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main() {
+    int n[4],
+        q = 0;
+    printf("Enter number(eg.'32 14 65 78'): ");
+    scanf("%d %d %d %d", &n[0], &n[1], &n[2], &n[3]);
+    for (int i=0;i<4;i++) {
+        if (n[i]%2 == 0) q++;
+    }
+    printf("There're %d even numbers", q);
+    return 0;
+}
