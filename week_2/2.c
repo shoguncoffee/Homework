61d2d718c2503c028d979512573e6ba4189589d1
shoguncoffee <63220735+shoguncoffee@users.noreply.github.com>
2022-08-15 23:04:57 +0700

    Update 2.c

--- week_2/2.c
+++ week_2/2.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main() {
+    int q = 4;
+    for (int i=0;i<4;i++) {
+        int x;
+        printf("Enter %d number: ", i+1);
+        scanf("%d", &x);
+        if (x%2) q--;
+    }
+    printf("There're %d even numbers", q);
+    return 0;
+}
