e801eb4511226a8ca1549aefb1ae490ad7c4ab1f
shoguncoffee <63220735+shoguncoffee@users.noreply.github.com>
2022-08-22 23:27:28 +0700

    Update 1.c

--- week_3/1.c
+++ week_3/1.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+int main() {
+    int q,w,e,z,x,c;
+    printf("Enter 1 number: ");
+    scanf("%d", &q);
+    printf("Enter 2 number: ");
+    scanf("%d", &w);
+    printf("Enter 3 number: ");
+    scanf("%d", &e);
+    z = q+e;
+    x = q+w;
+    c = w+e;
+    if (z>x && z>c) printf("%d + %d = %d", q, e, z);
+    else if (x>z && x>c) printf("%d + %d = %d", q, w, x);
+    else printf("%d + %d = %d", w, e, c);
+}
