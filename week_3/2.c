972cb37e682133ac7210fb59f2e2d910f7845f05
shoguncoffee <63220735+shoguncoffee@users.noreply.github.com>
2022-08-23 00:19:20 +0700

    Update 2.c

--- week_3/2.c
+++ week_3/2.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+int main() {
+    int num[2] = {0};
+    for (int i=0;i<3;i++) {
+      printf("(%d) Enter number: ", i+1);
+      int x, y=0;
+      scanf("%d", &x);
+      for (int n=0;n<2;n++) if (num[y] > num[n]) y = n;
+      if (x > num[y]) num[y] = x;
+    }
+  printf("%d + %d = %d", num[0], num[1], num[0] + num[1]);
+}
