81c2eb88c1bbb6316d3d792dc4a33db5a04c00e6
shoguncoffee <63220735+shoguncoffee@users.noreply.github.com>
2022-08-26 08:44:01 +0700

    Update 2.c

--- week_4/2.c
+++ week_4/2.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int num;
+    scanf("%d", &num);
+    int n = num-1;
+    for (int i=n;i>=-n;i--) {
+        for (int k=-n;k<=n;k++) {
+            char c = (k >= -abs(i) && k <= abs(i))? '*':' ';
+            printf("%c ", c);
+        }
+        printf("\n");
+    }
+}
