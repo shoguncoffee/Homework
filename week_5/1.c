0c5b3e243628ac4f1c9a45e8a1829cb5ba24c9a7
shoguncoffee <63220735+shoguncoffee@users.noreply.github.com>
2022-09-06 00:56:52 +0700

    Update 1.c

--- week_5/1.c
+++ week_5/1.c
@@ -1,1 +1,9 @@
-
+#include <stdio.h>
+int main() {
+    FILE *f;
+    f = fopen("/Users/shogun/Desktop/ttest.txt", "w");
+    printf("Input data string:\n");
+    for (char c; c!='.'; fprintf(f, "%c", c))
+        c = getchar();
+    fclose(f);
+}
