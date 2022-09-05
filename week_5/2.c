c0f70dede1d24758133901216a55d9909057fb6b
shoguncoffee <63220735+shoguncoffee@users.noreply.github.com>
2022-09-06 01:05:38 +0700

    Update 2.c

--- week_5/2.c
+++ week_5/2.c
@@ -2,11 +2,11 @@
 int main() {
     FILE *f;
     f = fopen("/Users/shogun/Desktop/ttest.txt", "w");
-    printf("Input data string:\n");
+    printf("Input data string:");
     while (1) {
         char c[512];
         scanf("%s", c);
-        fprintf(f, "%s\n", c);
+        fprintf(f, "\n%s", c);
         if (c[0] == '.' && c[1] == '\0') break;
     }
     fclose(f);
