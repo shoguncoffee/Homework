d5717f6cbf59e40bbe8c31ff3d15885eb37fd6cf
shoguncoffee <63220735+shoguncoffee@users.noreply.github.com>
2022-10-09 16:43:37 +0700

    Update 2.c

--- week_8/2.c
+++ week_8/2.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main() {
+    int v;
+    scanf("%d", &v);
+    for (int i = 2; i<v; i++) {
+        if (v % i == 0) {
+            printf("that is't prime number");
+            return;
+        }
+    }
+    printf("that's prime number");
+}
