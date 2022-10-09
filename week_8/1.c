414fc63ffd0a6aa422ef923726e73763ed3d30bc
shoguncoffee <63220735+shoguncoffee@users.noreply.github.com>
2022-10-09 16:17:16 +0700

    Update 1.c

--- week_8/1.c
+++ week_8/1.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <math.h>
 
+int main() {
+    int value;
+    scanf("%d", &value);
+    int p[2000] = {0}, now = 0;
+    for (int n = 2; n <= value; n++) {
+        for (int x = 0; x <= now; x++) {
+            if (p[x] == 0) {
+                p[now] = n;
+                now++;
+                break;
+            }
+            else if (n % p[x] == 0) 
+                break;
+        }
+    }
+    if (p[now-1] == value) 
+        printf("that's prime number");
+    else 
+        printf("that is't prime number");
+}
