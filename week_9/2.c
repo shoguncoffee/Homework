88232152aa81b2b4208b0071ddcf9a949e24513b
shoguncoffee <63220735+shoguncoffee@users.noreply.github.com>
2022-10-09 16:55:18 +0700

    Update 2.c

--- week_9/2.c
+++ week_9/2.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int isprime(int value) {
+    for (int i = 2; i<value; i++)
+        if (value % i == 0) return 0;
+    return 1;
+}
+
+int main() {
+    int v;
+    while (1) {
+        scanf("%d", &v);
+        if (isprime(v)) break;
+    }
+    printf("that's prime number");
+}
